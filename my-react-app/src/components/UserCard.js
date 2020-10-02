2b9183c787c69b9f962342717ac5d4625daf41ff
Heather Beckman <heatherbeckman85@gmail.com>
2020-10-02 13:32:11 -0600

    mvp met

--- my-react-app/src/components/UserCard.js
+++ my-react-app/src/components/UserCard.js
@@ -5,8 +5,10 @@ const UserCard = (props) => {
     return (
 
     <div className="cards">
+        <a href={props.user.html_url} target="_blank">
           <img width="200px" src={props.user.avatar_url}/>
           <h2>{props.user.login}</h2>
+        </a>
     </div>
 
     )}
